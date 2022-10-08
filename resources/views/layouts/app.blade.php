1308cdb14c5949d98856dfa699db3649fee3d69e
sarev17 <sarev17@gmail.com>
2022-10-08 05:57:26 -0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="//js/masks.js"></script>
-        <script src="//js/functions.js"></script>
-        <script src="//js/events.js"></script>
+        <script src="/js/masks.js"></script>
+        <script src="/js/functions.js"></script>
+        <script src="/js/events.js"></script>
 
         <!-- Scripts -->
       @vite(['resources/sass/app.scss', 'resources/js/app.js'])
